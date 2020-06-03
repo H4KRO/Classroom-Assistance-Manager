5db263f6d31d0d73a83a80d0e66462b289736cbe
H4KRO <>
2020-06-03 09:16:54 +0200

    TCP Components

---
+++ src/server/TCPClientHandler.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import commons.TCPHandler;
+
+public class TCPClientHandler extends TCPHandler {
+	
+}
---
+++ src/server/TCPServer.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class TCPServer {
+	
+}
