3d481b15160cde15e7e5bac5a4b385f0bc137aa9
H4KRO <>
2020-06-09 14:54:28 +0200

    remove imports

--- src/client/MainClient.java
+++ src/client/MainClient.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import TCP.TCPHandler;
-
 public class MainClient {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -2,8 +2,6 @@ package server;
 
 import java.io.IOException;
 
-import TCP.TCPServer;
-
 public class MainServer {
 
 	public static void main(String[] args) throws IOException {
